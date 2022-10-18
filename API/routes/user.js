e0bf766625f4a53cfc11086a174ed6a032ad585f
kaaf7 <amer.m.karim@gmail.com>
2022-10-18 15:11:47 +0200

    init

--- API/routes/user.js
+++ API/routes/user.js
@@ -1,4 +1,7 @@
-const { verifyTokenAndAuthorization,verifyTokenAndAdmin } = require("./verifytoken");
+const {
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifytoken");
 const CryptoJS = require("crypto-js");
 const dotenv = require("dotenv");
 const User = require("../models/User");
@@ -11,7 +14,7 @@ router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
     const { password, ...others } = user._doc;
-    res.status(200).json(others)
+    res.status(200).json(others);
   } catch (err) {
     res.status(500).json("error");
   }
@@ -19,17 +22,44 @@ router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
 
 // GET ALL USERS
 
-
-router.get("/", verifyTokenAndAdmin, async (req, res) => {
+router.get("/users", verifyTokenAndAdmin, async (req, res) => {
+  const query = req.query.new;
   try {
-    const users = await User.find();
-    res.status(200).json(users)
+    const users = query
+      ? await User.find().sort({ _id: -1 }).limit(5)
+      : await User.find();
+    res.status(200).json(users);
   } catch (err) {
     res.status(500).json("error");
   }
 });
 
+// Get user stats
 
+router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
+  const date = new Date();
+  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+  try {
+    const data = await User.aggregate([
+      { $match: { createdAt: { $gte: lastYear } } },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+    res.status(200).json(data)
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.post("/:id", (req, res) => {});
 
@@ -59,7 +89,7 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
 router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
   try {
     await User.findByIdAndDelete(req.params.id);
-    res.status(200).json("user has been deleted")
+    res.status(200).json("user has been deleted");
   } catch (err) {
     res.status(600).json("error");
   }
