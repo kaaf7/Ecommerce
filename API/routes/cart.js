a19cef24d21a632be6907d018413e0e525fa0ccf
kaaf7 <amer.m.karim@gmail.com>
2022-10-18 19:00:37 +0200

    init

--- API/routes/cart.js
+++ API/routes/cart.js
@@ -1,5 +1,29 @@
 const router = require("express").Router();
 
-router.get("/cart", (req, res) => {});
+const Cart = require("../models/Cart");
+const {
+  verifyJwtToken,
+  verifyTokenAndAdmin,
+  verifyTokenAndAuthorization,
+} = require("./verifytoken");
+
+router.post("/id:", verifyTokenAndAuthorization, async (req, res) => {
+  const newCart = new Cart(req.body);
+  try {
+    const savedCart = await newCart.save();
+    res.status(200).json(savedCart);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+//get cart
+router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    const cart = await Cart.findOne({ userId: req.params.userId });
+    req.status(200).json(cart);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
