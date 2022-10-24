fbe9c99d7f1e9215923f8bbfb7149b2c3364861d
kaaf7 <amer.m.karim@gmail.com>
2022-10-24 16:56:12 +0200

    :iphone: Add responsive design to the Highlight

--- src/Components/Highlights.jsx
+++ src/Components/Highlights.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import HighlightCard from "./HighlightCard";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
   height: 100vh;
@@ -10,16 +11,27 @@ const Container = styled.div`
   justify-content: center;
   align-items: center;
   background-color: #c7c7c71d;
+  ${mobile({
+    height: "100vh",
+    marginBottom: "10vh",
+  })}
 `;
 const Wrapper = styled.div`
   height: 100%;
-  gap:25px;
+  gap: 25px;
   display: flex;
   justify-content: space-evenly;
   align-items: center;
   margin-left: 150px;
   margin-right: 150px;
-  
+  ${mobile({
+    marginLeft: "1vw",
+    marginRight: "1vw",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "100%",
+  })}
 `;
 const Highlights = () => {
   return (
@@ -28,10 +40,11 @@ const Highlights = () => {
         <HighlightCard
           color="red"
           link="https://lp.cosstores.com/app001prod?set=source[/52/06/52066288e5d14f8ef1182c77de0e88a0ddd50f5a.jpg],origin[dam],type[LOOKBOOK],device[hdpi],quality[80],ImageVersion[1]&call=url[file:/product/main]"
-        ></HighlightCard>
-        <HighlightCard link="https://lp.cosstores.com/app001prod?set=source[/2a/d8/2ad8b54b100ea1274c8ac14ff53939745ca5569f.jpg],origin[dam],type[LOOKBOOK],device[hdpi],quality[80],ImageVersion[1]&call=url[file:/product/main]"></HighlightCard>
-        <HighlightCard link="https://lp.cosstores.com/app001prod?set=source[/67/a9/67a9a76a301509f2a140eb82e9fd3cb2442a7fe6.jpg],origin[dam],type[LOOKBOOK],device[hdpi],quality[80],ImageVersion[2]&call=url[file:/product/main]"></HighlightCard>
-        <HighlightCard link="https://lp.cosstores.com/app001prod?set=source[/7e/a7/7ea78bf746257b6db2878e1d8ff56d7f37f15a28.jpg],origin[dam],type[LOOKBOOK],device[hdpi],quality[80],ImageVersion[1]&call=url[file:/product/main]"></HighlightCard>
+        ></HighlightCard>{" "}
+        <HighlightCard
+          color="red"
+          link="https://lp.cosstores.com/app001prod?set=source[/52/06/52066288e5d14f8ef1182c77de0e88a0ddd50f5a.jpg],origin[dam],type[LOOKBOOK],device[hdpi],quality[80],ImageVersion[1]&call=url[file:/product/main]"
+        ></HighlightCard>{" "}
       </Wrapper>
     </Container>
   );
