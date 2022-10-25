3e8a9c8d3f4e04b9b4423a7b68882578277b36a6
kaaf7 <amer.m.karim@gmail.com>
2022-10-25 16:58:23 +0200

    :pen: Adjust Product Schema info

--- API/models/Product.js
+++ API/models/Product.js
@@ -1,16 +1,19 @@
 const mongoose = require("mongoose");
+
+/* create mongoDB schema for Products with every essential detail*/
 const ProductSchema = new mongoose.Schema(
   {
-    title: { type: String, required: true, unique: true },
-    description: { type: String, required: true},
+    productTitle: { type: String, required: true, unique: false },
+    description: { type: String, required: true },
     category: { type: Array, required: true },
     price: { type: Number, required: true },
     colors: { type: Array, required: true },
     favorite: { type: Boolean, default: false },
     isNew: { type: Boolean, default: true },
+    images: { type: Array, required: true },
+    video: { typye: String, required: false },
   },
   { timestamps: true }
 );
 
 module.exports = mongoose.model("Product", ProductSchema);
-
