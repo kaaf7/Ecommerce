9f03c4269c3ecfbfab8b379e2589596c08aab306
kaaf7 <amer.m.karim@gmail.com>
2022-10-31 20:04:28 +0100

    Add new Routes and Pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from "react";
 import Home from "./pages/Home";
 import ProductPage from "./pages/ProductPage";
-import Products from"./pages/Products"
+import Products from "./pages/Products";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
 function App() {
   return (
     <Router>
       <Routes>
-      <Route exact path="/" element={<Home/>}/>
-      <Route path="product/:id" element={<ProductPage/>}/>
-      <Route path="/products/:category" element={<Products/>}/>
+        <Route exact path="/" element={<Home />} />
+        <Route path="product/:id" element={<ProductPage />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/men" element={<Products />} />
+        <Route path="/products/women" element={<Products />} />
+        <Route path="/products/new" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
