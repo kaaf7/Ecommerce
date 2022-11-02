15fae806f1e6715c1e9968774022c84f41009220
kaaf7 <amer.m.karim@gmail.com>
2022-11-03 00:31:47 +0100

    Get products from API using axios

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,182 +1,9 @@
 import React from "react";
 import ProductCard from "./ProductCard";
+import { useState, useEffect } from "react";
 import styled from "styled-components";
-import { v4 as uuidv4 } from "uuid";
+import axios from "axios";
 
-const uniqueId = uuidv4();
-
-
-//dummy data
-const productArray = [
-  {
-    title: "prouct",
-    price: "15",
-    id: uniqueId,
-    images: [ 
-      "https://iili.io/DXRnKG.jpg",
-      "https://iili.io/DXRIHl.jpg",
-      "https://iili.io/DXRolf.jpg",
-    ],
-    colors: ["yellow", "green", "black"],
-    category: ["men", "women"],
-  },
-  {
-    title: "prouct",
-    price: "95",
-    id: uniqueId,
-    images: [
-      "https://iili.io/DXRnKG.jpg",
-      "https://iili.io/DXRIHl.jpg",
-      "https://iili.io/DXRolf.jpg",
-    ],
-    colors: ["yellow", "green", "black"],
-    category: ["men", "women"],
-  },
-  {
-    title: "prouct",
-    id: uniqueId,
-    price: "35",
-    images: [
-      "https://iili.io/DXRnKG.jpg",
-      "https://iili.io/DXRIHl.jpg",
-      "https://iili.io/DXRolf.jpg",
-    ],
-    colors: ["yellow", "green", "black"],
-    category: ["men", "women"],
-  },
-  {
-    title: "prouct",
-    price: "52",
-    id: uniqueId,
-    images: [
-      "https://iili.io/DXRnKG.jpg",
-      "https://iili.io/DXRIHl.jpg",
-      "https://iili.io/DXRolf.jpg",
-    ],
-    colors: ["yellow", "green", "black"],
-    category: ["men", "women"],
-  },
-  {
-    title: "prouct",
-    price: "95",
-    id: uniqueId,
-    images: [
-      "https://iili.io/DXRnKG.jpg",
-      "https://iili.io/DXRIHl.jpg",
-      "https://iili.io/DXRolf.jpg",
-    ],
-    colors: ["yellow", "green", "black"],
-    category: ["men", "women"],
-  },
-  {
-    title: "prouct",
-    id: uniqueId,
-    price: "35",
-    images: [
-      "https://iili.io/DXRnKG.jpg",
-      "https://iili.io/DXRIHl.jpg",
-      "https://iili.io/DXRolf.jpg",
-    ],
-    colors: ["yellow", "green", "black"],
-    category: ["men", "women"],
-  },
-  {
-    title: "prouct",
-    price: "52",
-    id: uniqueId,
-    images: [
-      "https://iili.io/DXRnKG.jpg",
-      "https://iili.io/DXRIHl.jpg",
-      "https://iili.io/DXRolf.jpg",
-    ],
-    colors: ["yellow", "green", "black"],
-    category: ["men", "women"],
-  },
-  {
-    title: "prouct",
-    price: "15",
-    id: uniqueId,
-    images: [
-      "https://iili.io/DXRnKG.jpg",
-      "https://iili.io/DXRIHl.jpg",
-      "https://iili.io/DXRolf.jpg",
-    ],
-    colors: ["yellow", "green", "black"],
-    category: ["men", "women"],
-  },
-  {
-    title: "prouct",
-    price: "95",
-    id: uniqueId,
-    images: [
-      "https://iili.io/DXRnKG.jpg",
-      "https://iili.io/DXRIHl.jpg",
-      "https://iili.io/DXRolf.jpg",
-    ],
-    colors: ["yellow", "green", "black"],
-    category: ["men", "women"],
-  },
-  {
-    title: "prouct",
-    id:uniqueId,
-    price: "35",
-    images: [
-      "https://iili.io/DXRnKG.jpg",
-      "https://iili.io/DXRIHl.jpg",
-      "https://iili.io/DXRolf.jpg",
-    ],
-    colors: ["yellow", "green", "black"],
-    category: ["men", "women"],
-  },
-  {
-    title: "prouct",
-    price: "52",
-    id: uniqueId,
-    images: [
-      "https://iili.io/DXRnKG.jpg",
-      "https://iili.io/DXRIHl.jpg",
-      "https://iili.io/DXRolf.jpg",
-    ],
-    colors: ["yellow", "green", "black"],
-    category: ["men", "women"],
-  },
-  {
-    title: "prouct",
-    price: "95",
-    id: uniqueId,
-    images: [
-      "https://iili.io/DXRnKG.jpg",
-      "https://iili.io/DXRIHl.jpg",
-      "https://iili.io/DXRolf.jpg",
-    ],
-    colors: ["yellow", "green", "black"],
-    category: ["men", "women"],
-  },
-  {
-    title: "prouct",
-    id: uniqueId,
-    price: "35",
-    images: [
-      "https://iili.io/DXRnKG.jpg",
-      "https://iili.io/DXRIHl.jpg",
-      "https://iili.io/DXRolf.jpg",
-    ],
-    colors: ["yellow", "green", "black"],
-    category: ["men", "women"],
-  },
-  {
-    title: "prouct",
-    price: "52",
-    id: uniqueId,
-    images: [
-      "https://iili.io/DXRnKG.jpg",
-      "https://iili.io/DXRIHl.jpg",
-      "https://iili.io/DXRolf.jpg",
-    ],
-    colors: ["yellow", "green", "black"],
-    category: ["men", "women"],
-  },
-];
 const Wrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
@@ -188,12 +15,56 @@ const Wrapper = styled.div`
   gap: 25px;
   position: relative;
 `;
-const ProductList = () => {
+const ProductList = ({ filters, cat, sort }) => {
+  console.log(filters, cat, sort);
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get(
+          cat
+            ? `http://localhost:3005/api/products/allproducts?category=${cat}`
+            : "http://localhost:3005/api/products/allproducts"
+        );
+        setProducts(res.data);
+      } catch (err) {}
+    };
+    getProducts();
+  }, [cat]);
+
+  useEffect(() => {
+    if (filters) {
+      setFilteredProducts(
+        products.filter((item) =>
+          Object.entries(filters).every(([key, value]) =>
+            item[key].includes(value.toLowerCase())
+          )
+        )
+      );
+    }
+  }, [products, cat, filters]);
+
+  useEffect(() => {
+    if (sort === "ascending") {
+      setFilteredProducts((products) =>
+        [...products].sort((a, b) => a.price - b.price)
+      );
+    } else {
+      setFilteredProducts((products) =>
+        [...products].sort((a, b) => b.price - a.price)
+      );
+    }
+  }, [sort]);
+
+
   return (
     <Wrapper>
-      {productArray.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
+      {filters
+        ? filteredProducts.map((product) => (
+            <ProductCard  key={product._id} product={product} />
+          ))
+        : setFilteredProducts(products)}
     </Wrapper>
   );
 };
