f1dbdf9b5f23e7ff8199cc33415388aced138ef1
kaaf7 <amer.m.karim@gmail.com>
2022-11-13 23:04:02 +0100

    Arrange Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 import styled from "styled-components";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
   height: 30vh;
   display: flex;
   width: 100%;
   justify-content: space-between;
-  background-color: #f9f9f9;
   overflow: hidden;
+  ${mobile({
+    display: "flex",
+    justifyContent: "flex-start", 
+    height: "15vh",
+    backgroundColor:"yellow",
+  
+  })}
 `;
-
 const Items = styled.div`
   font-size: 12px;
   color: black;
   cursor: pointer;
   flex: 1;
+  ${mobile({
+    fontSize: "10px",
+  })}
 `;
 const LeftItems = styled.div`
   margin-left: 100px;
@@ -25,6 +34,9 @@ const LeftItems = styled.div`
   max-width: 30%;
   gap: 10px;
   flex: 1;
+  ${mobile({
+    display: "none",
+  })}
 `;
 const RightItems = styled.div`
   margin-right: 100px;
@@ -46,74 +58,73 @@ const ItemsContainer = styled.div`
   max-width: 30%;
   flex: 1;
 `;
-
 const Footer = () => {
   return (
     <Container>
-        <LeftItems>
-          <ItemsContainer>
-            <Items>iverra quis, feugiat a, tellus. Phasellus v</Items>
-            <Items>tellus. Phasellus v</Items>
-            <Items>iverra quis, feugiat a, tellus. Phasellus v</Items>
-            <Items>iverra quis, feugiat a, tellus. Phasellus v</Items>
-            <Items>Text</Items>
-            <Items>Text</Items>
-            <Items>Text</Items>
-            <Items>Text</Items>
-            <Items>Text</Items>
-            <Items>Text</Items>
-          </ItemsContainer>
-          <ItemsContainer>
-            <Items>Kontakt</Items>
-            <Items>Lieferung</Items>
-            <Items>Text</Items>
-            <Items>Text</Items>
-            <Items>Text</Items>
-            <Items>Text</Items>
-            <Items>Text</Items>
-            <Items>Text</Items>
-            <Items>Text</Items>
-            <Items>Text</Items>
-          </ItemsContainer>
-          <ItemsContainer>
-            <Items>Kontakt</Items>
-            <Items>Lieferung</Items>
-            <Items>Text</Items>
-            <Items>Text</Items>
-            <Items>Text</Items>
-            <Items>Text</Items>
-            <Items>Text</Items>
-            <Items>Text</Items>
-            <Items>Text</Items>
-            <Items>Text</Items>
-          </ItemsContainer>
-        </LeftItems>
-        <RightItems>
-          <ItemsContainer>
-            <Items>Kontakt</Items>
-            <Items>Lieferung</Items>
-            <Items>Text</Items>
-            <Items>Text</Items>
-            <Items>Text</Items>
-            <Items>Text</Items>
-            <Items>Text</Items>
-            <Items>Text</Items>
-            <Items>Text</Items>
-            <Items>Text</Items>
-          </ItemsContainer>
-          <ItemsContainer>
-            <Items>Kontakt</Items>
-            <Items>Lieferung</Items>
-            <Items>Text</Items>
-            <Items>Text</Items>
-            <Items>Text</Items>
-            <Items>Text</Items>
-            <Items>Text</Items>
-            <Items>Text</Items>
-            <Items>Text</Items>
-            <Items>Text</Items>
-          </ItemsContainer>
-        </RightItems>
+      <LeftItems>
+        <ItemsContainer>
+          <Items>iverra quis, feugiat a, tellus. Phasellus v</Items>
+          <Items>tellus. Phasellus v</Items>
+          <Items>iverra quis, feugiat a, tellus. Phasellus v</Items>
+          <Items>iverra quis, feugiat a, tellus. Phasellus v</Items>
+          <Items>Text</Items>
+          <Items>Text</Items>
+          <Items>Text</Items>
+          <Items>Text</Items>
+          <Items>Text</Items>
+          <Items>Text</Items>
+        </ItemsContainer>
+        <ItemsContainer>
+          <Items>Kontakt</Items>
+          <Items>Lieferung</Items>
+          <Items>Text</Items>
+          <Items>Text</Items>
+          <Items>Text</Items>
+          <Items>Text</Items>
+          <Items>Text</Items>
+          <Items>Text</Items>
+          <Items>Text</Items>
+          <Items>Text</Items>
+        </ItemsContainer>
+        <ItemsContainer>
+          <Items>Kontakt</Items>
+          <Items>Lieferung</Items>
+          <Items>Text</Items>
+          <Items>Text</Items>
+          <Items>Text</Items>
+          <Items>Text</Items>
+          <Items>Text</Items>
+          <Items>Text</Items>
+          <Items>Text</Items>
+          <Items>Text</Items>
+        </ItemsContainer>
+      </LeftItems>
+      <RightItems>
+        <ItemsContainer>
+          <Items>Kontakt</Items>
+          <Items>Lieferung</Items>
+          <Items>Text</Items>
+          <Items>Text</Items>
+          <Items>Text</Items>
+          <Items>Text</Items>
+          <Items>Text</Items>
+          <Items>Text</Items>
+          <Items>Text</Items>
+          <Items>Text</Items>
+        </ItemsContainer>
+        <ItemsContainer>
+          <Items>Kontakt</Items>
+          <Items>Lieferung</Items>
+          <Items>Text</Items>
+          <Items>Text</Items>
+          <Items>Text</Items>
+          <Items>Text</Items>
+          <Items>Text</Items>
+          <Items>Text</Items>
+          <Items>Text</Items>
+          <Items>Text</Items>
+        </ItemsContainer>
+      </RightItems>
     </Container>
   );
 };
