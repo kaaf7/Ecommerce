3ff5f8bc0c40cf2ec2885df9483ef5d3a25b6c0c
kaaf7 <amer.m.karim@gmail.com>
2022-11-14 18:00:36 +0100

    Add Provider from reac-redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-const container = document.getElementById('root')
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import  store  from "./redux/store";
+const container = document.getElementById("root");
+
 
 const root = ReactDOM.createRoot(container);
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
