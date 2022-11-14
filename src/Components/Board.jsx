54584d92cea6b852046aba987ae71ab9c3841231
kaaf7 <amer.m.karim@gmail.com>
2022-11-14 17:41:23 +0100

    Arrange Board and add comments

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -1,12 +1,25 @@
+/* 👇 This is Board display Component  
+created and styled with Styled Component it will display 
+3 random product photos and a fashion quote	
+cretaed and styled with Styled Component 
+*/
+
 import React from "react";
+
 import styled from "styled-components";
-import { mobile } from "../responsive";
+
 import { useState, useEffect } from "react";
+
+// import Navigate to redirect to pages
 import { useNavigate } from "react-router-dom";
 
+//import responsive Settings from responsive.js
+import { mobile } from "../responsive";
+
+//import Axios Library to get data
 import axios from "axios";
 
-
+// Array of random quotes
 const dummyQuotes = [
   "YOU CAN HAVE ANYTHING YOU WANT IN LIFE IF YOU DRESS FOR IT",
   "The joy of dressing is an art ",
@@ -15,30 +28,35 @@ const dummyQuotes = [
   "We must never confuse elegance with snobbery.",
 ];
 
+// Components Container
 const Container = styled.div`
-  height: 100vh;
   width: 100%;
+  height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   border-bottom: 1px solid lightgray;
   border-top: 1px solid lightgray;
 `;
+
+// Components Wrapper
 const Wrapper = styled.div`
-  display: flex;
   height: 90%;
-  overflow: hidden;
+  display: flex;
   justify-content: center;
   align-items: center;
+  overflow: hidden;
   margin-left: 150px;
   margin-right: 150px;
   ${mobile({
     height: "100vh",
   })}
 `;
+
+// Image Component to display product images
 const Image = styled.img`
-  flex: 1;
   height: 90%;
+  flex: 1;
   object-fit: cover;
   filter: grayscale(1);
   transition: 0.1s ease-out;
@@ -55,13 +73,15 @@ const Image = styled.img`
     width: "90vw",
   })}
 `;
+
+// Quote Component to display random quotes
 const Quote = styled.p`
-  position: absolute;
   margin-top: 0%;
   margin-left: 0%;
-  font-size: 30px;
-  color: #e13913;
+  position: absolute;
+  font-size: 40px;
   font-weight: 600;
+  color: #e13913;
   z-index: 1;
   ${mobile({
     fontSize: "20px",
@@ -70,6 +90,16 @@ const Quote = styled.p`
 `;
 
 const Board = () => {
+  /*useNavigate to switch pages*/
+  const navigate = useNavigate();
+
+  /*OpenProduct function to open any of the products displayd based on productId*/
+  const openProduct = (productId) => {
+    navigate(`/products/${productId}`);
+  };
+
+  /*useState to get random products from the database 
+    and display only 3 random products each tme*/
   const [products, setProducts] = useState([]);
   useEffect(() => {
     const getProducts = async () => {
@@ -78,33 +108,29 @@ const Board = () => {
           "http://localhost:3005/api/products/allproducts"
         );
         setProducts(res.data);
-        console.log(res.data);
       } catch (err) {}
     };
     getProducts();
   }, []);
 
-
-
-    const navigate = useNavigate();
-    const openProduct = (productId) => {
-      navigate(`/products/${productId}`);
-    };
-  
   return (
     <Container>
       <Wrapper>
-        <Quote>
-          {dummyQuotes
-            .sort(() => 0.5 - Math.random())
-            .slice(0, 1)
-            .map((quote) => quote.toUpperCase())}
-        </Quote>
+        {dummyQuotes
+          .sort(() => 0.5 - Math.random())
+          .slice(0, 1)
+          .map((quote) => (
+            <Quote key={quote}>{quote.toUpperCase()}</Quote>
+          ))}
         {products
           .sort(() => 0.5 - Math.random())
           .slice(0, 3)
           .map((product) => (
-            <Image onClick={() => openProduct(product._id)} src={product.images[1]}></Image>
+            <Image
+              key={product._id}
+              onClick={() => openProduct(product._id)}
+              src={product.images[1]}
+            ></Image>
           ))}
       </Wrapper>
     </Container>
