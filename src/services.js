96ac7f31945648d1546962db2773ae350d76bf3d
kaaf7 <amer.m.karim@gmail.com>
2022-11-14 20:13:46 +0100

    Add axios request services files

---
+++ src/services.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = process.env.BASE_URL;
+
+const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
+const currentUser = user && JSON.parse(user).currentUser;
+const TOKEN = currentUser?.accessToken;
+
+export const publicRequest = axios.create({
+  baseURL: BASE_URL,
+});
+
+export const servicesRequest = axios.create({
+  baseURL: BASE_URL,
+  header: { token: `${TOKEN}` },
+});
