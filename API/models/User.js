48463a7f3218eadd7005bab4d82873f95ff1861a
kaaf7 <amer.m.karim@gmail.com>
2022-11-14 17:57:34 +0100

    Edit Product model

--- API/models/User.js
+++ API/models/User.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true, unique: true, unique: true },
-    email: { type: String, required: true, unique: true, unique: true },
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     isAdmin: { type: Boolean, default: false },
   },
