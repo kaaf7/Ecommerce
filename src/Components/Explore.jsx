159233f3275459b79cad427359ab3ae0cfa662fc
kaaf7 <amer.m.karim@gmail.com>
2022-11-14 17:47:22 +0100

    Arrange Explore and add comments

--- src/Components/Explore.jsx
+++ src/Components/Explore.jsx
@@ -1,55 +1,74 @@
+/* 👇 This is Explore display Component
+created and styled with Styled Components,
+it will redirect customer to other pages with products 
+*/
+
 import React from "react";
+
 import styled from "styled-components";
+
+//import responsive Settings from responsive.js
 import { mobile } from "../responsive";
+
+// import Navigate to redirect to pages
 import { useNavigate } from "react-router-dom";
 
+// all Components Container
 const Container = styled.div`
+  width: 100%;
+  height: 25vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  height: 25vh;
-  width: 100%;
   ${mobile({
     height: "15vh",
   })}
 `;
+
+// Components Wrapper
 const Wrapper = styled.div`
-  flex: 1;
   display: flex;
   flex-direction: column;
   justify-content: flex-end;
+  flex: 1;
   align-items: center;
   text-align: center;
   max-height: 80%;
   overflow: hidden;
 `;
+
+// Explore Component Title
 const Title = styled.h1`
   margin-bottom: 100px;
   margin-top: 10px;
-  font-size: 35px;
   margin-bottom: 0px;
-  color: #272727f5;
+  font-size: 35px;
   font-weight: bolder;
+  color: #272727f5;
 `;
-const Buttons = styled.div`
+
+// Button container
+const ButtonsContainer = styled.div`
+  max-width: 50%;
   display: flex;
   justify-content: center;
-  gap: 10px;
   align-items: top;
   flex: 1;
-  max-width: 50%;
+  gap: 10px;
   ${mobile({
     gap: "20px",
   })}
 `;
+
+//Button to redirect to products page
 const Button = styled.button`
-  margin-top: 20px;
   width: 20vw;
   height: 4vh;
-  background-color: transparent;
+  margin-top: 20px;
   border: 0.1px solid lightgrey;
   font-size: 14px;
   color: #272727f5;
+  background-color: transparent;
   cursor: pointer;
   ${mobile({
     fontSize: "10px",
@@ -58,7 +77,10 @@ const Button = styled.button`
 `;
 
 const Explore = () => {
+  /*useNavigate to switch pages*/
   const navigate = useNavigate();
+  
+  /*NavigateDir depends on custome directory*/
   const NavigateDir = (directory) => {
     navigate(directory);
   };
@@ -66,7 +88,7 @@ const Explore = () => {
     <Container>
       <Wrapper>
         <Title>EXPLORE MORE</Title>
-        <Buttons>
+        <ButtonsContainer>
           <Button
             onClick={() => {
               NavigateDir("/products/men");
@@ -95,7 +117,7 @@ const Explore = () => {
           >
             FAVORITES
           </Button>
-        </Buttons>
+        </ButtonsContainer>
       </Wrapper>
     </Container>
   );
