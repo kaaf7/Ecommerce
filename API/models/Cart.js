6473562577c4f2da6a90423ab9b5cd96239c785e
kaaf7 <amer.m.karim@gmail.com>
2022-11-23 19:50:20 +0100

    Edit Cart Schema

--- API/models/Cart.js
+++ API/models/Cart.js
@@ -2,19 +2,9 @@ const mongoose = require("mongoose");
 /* create mongoDB schema for Cart with every essential detail*/
 const CartSchema = new mongoose.Schema(
   {
-    userId: { type: String, required: false },
-    products: [
-      {
-        productId: { type: String },
-        quantity: {
-          type: Number,
-          required: true,
-        },
-        price: { type: Number, required: true },
-        color: { type: String, required: true },
-        size: { type: String, required: true },
-      },
-    ],
+    userId: { type: String, required: true },
+    products: [],
+    quantity: { type: Number, required: true },
   },
   { timestamps: true }
 );
@@ -23,9 +13,8 @@ CartSchema.set("toJSON", {
   transform: function (document, returnedObject) {
     returnedObject.id = returnedObject._id;
     delete returnedObject._id;
-     delete returnedObject.__v;
-     delete returnedObject.id;
-    returnedObject.products.forEach((item) => delete item["_id"]);
+    delete returnedObject.__v;
+    delete returnedObject.id;
   },
 });
 
