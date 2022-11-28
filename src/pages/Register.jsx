ef525865d3a8500a0402b53ba9ef354750d6c9c5
kaaf7 <amer.m.karim@gmail.com>
2022-11-28 21:34:26 +0100

    Edit Register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,15 +1,13 @@
 import styled from "styled-components";
-import {mobile} from "../responsive";
+import { useDispatch } from "react-redux";
+import { mobile } from "../responsive";
+import { useState } from "react";
+import { register } from "../redux/apiCalls";
 
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
-  background: linear-gradient(
-      rgba(255, 255, 255, 0.5),
-      rgba(255, 255, 255, 0.5)
-    ),
-    url("https://images.pexels.com/photos/6984661/pexels-photo-6984661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
-      center;
+
   background-size: cover;
   display: flex;
   align-items: center;
@@ -26,8 +24,9 @@ const Wrapper = styled.div`
 const Title = styled.h1`
   font-size: 24px;
   font-weight: 300;
+  color: #555555;
+  font-weight: bolder;
 `;
-
 const Form = styled.form`
   display: flex;
   flex-wrap: wrap;
@@ -49,28 +48,51 @@ const Button = styled.button`
   width: 40%;
   border: none;
   padding: 15px 20px;
-  background-color: teal;
+  background-color: #640000;
   color: white;
   cursor: pointer;
 `;
 
 const Register = () => {
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const handleRegister = (e) => {
+    e.preventDefault();
+    register(dispatch, { email, username, password });
+  };
+
   return (
     <Container>
       <Wrapper>
         <Title>CREATE AN ACCOUNT</Title>
         <Form>
-          <Input placeholder="name" />
-          <Input placeholder="last name" />
-          <Input placeholder="username" />
-          <Input placeholder="email" />
-          <Input placeholder="password" />
-          <Input placeholder="confirm password" />
+          <Input
+            placeholder="username"
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+          <Input
+            placeholder="email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <Input
+            type="password"
+            placeholder="password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <Input type="password" placeholder="confirm password" />
           <Agreement>
             By creating an account, I consent to the processing of my personal
             data in accordance with the <b>PRIVACY POLICY</b>
           </Agreement>
-          <Button>CREATE</Button>
+          <Button onClick={handleRegister}>CREATE</Button>
         </Form>
       </Wrapper>
     </Container>
