7aa181cb8efaab37bf082cc4b8a4e9786c0c22ce
kaaf7 <amer.m.karim@gmail.com>
2022-11-30 13:43:12 +0100

    Edit Products Page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import { Navbar } from "../Components/Navbar";
-import SaleSlider from "../Components/SaleSlider";
 import styled from "styled-components";
 import Footer from "../Components/Footer";
 import Explore from "../Components/Explore";
 import Board from "../Components/Board";
 import ProductList from "../Components/ProductList";
 
+
 const Container = styled.div`
-  background-color: #f9f9f9;
+  background-color: #ffffff;
 `;
 
 const FilterWrapper = styled.div`
@@ -18,12 +18,13 @@ const FilterWrapper = styled.div`
   align-items: center;
   gap: 50px;
   margin-left: 150px;
+  margin-top: 9vh;
   position: relative;
   height: 50px;
 `;
 
 const Filter = styled.select`
-  background-color: #f9f9f9;
+  background-color: #ffffff;
   border: none;
   border: none;
   font-size: 15px;
@@ -55,10 +56,9 @@ const Products = () => {
   return (
     <Container>
       <Navbar></Navbar>
-      <SaleSlider></SaleSlider>
       <FilterWrapper>
         <Filter name="colors" id="colors" onChange={handleFilters}>
-          <option disabled selected>
+          <option selected disabled>
             COLOR
           </option>
           <option>GREEN</option>
@@ -68,7 +68,7 @@ const Products = () => {
           <option>WHITE</option>
         </Filter>
         <Filter name="category" id="style" onChange={handleFilters}>
-          <option disabled selected>
+          <option selected disabled>
             STYLE
           </option>
           <option>JACKETS</option>
@@ -77,7 +77,7 @@ const Products = () => {
           <option>TROUSERS</option>
         </Filter>
         <Filter name="category" onChange={handleFilters}>
-          <option disabled selected>
+          <option selected disabled>
             SIZE
           </option>
           <option>S</option>
@@ -87,14 +87,14 @@ const Products = () => {
         </Filter>
 
         <Filter name="category" onChange={handleFilters}>
-          <option disabled selected>
+          <option selected disabled>
             CATEGORY
           </option>
           <option>WOMEN</option>
           <option>MEN</option>
         </Filter>
         <Filter onChange={handleSort}>
-          <option disabled selected>
+          <option selected disabled>
             PRICE
           </option>
           <option value="ascending">ASCENDING</option>
