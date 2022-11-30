bb921b02b5f9db6c31b9e4146b9155005bc401d8
kaaf7 <amer.m.karim@gmail.com>
2022-11-30 13:27:43 +0100

    Add favofite route

--- API/index.js
+++ API/index.js
@@ -6,10 +6,12 @@ const authRout = require("./routes/auth");
 const productRout = require("./routes/product");
 const orderRout = require("./routes/order");
 const cartRout = require("./routes/cart");
+const favoriteRout = require("./routes/favorite");
+
 const dotenv = require("dotenv");
-const cors = require('cors')
+const cors = require("cors");
 dotenv.config();
-app.use(cors())
+app.use(cors());
 const PORT = process.env.PORT;
 const URI = process.env.URI;
 app.use(express.json());
@@ -36,4 +38,4 @@ app.use("/api/auth", authRout);
 app.use("/api/products", productRout);
 app.use("/api/orders", orderRout);
 app.use("/api/cart", cartRout);
-
+app.use("/api/favorite", favoriteRout);
