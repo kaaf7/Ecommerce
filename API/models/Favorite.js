caea1677982b21a18875b1f2f40a6ac288c506d9
kaaf7 <amer.m.karim@gmail.com>
2022-12-08 19:11:58 +0100

    Add Comments to Favorite

--- API/models/Favorite.js
+++ API/models/Favorite.js
@@ -1,14 +1,21 @@
+/* * 👇
+ *This is Favorite Schema
+ *It will contain userId and an array of products, also quantity
+ *It will be used to get user's favorite items whenever they login
+ */
+
 const mongoose = require("mongoose");
-/* create mongoDB schema for favorite with every essential detail*/
+// create new shcema
 const FavoriteSchema = new mongoose.Schema(
   {
     userId: { type: String, required: true },
-    products: [],
+    favorites: [],
     quantity: { type: Number, required: false },
   },
   { timestamps: true }
 );
 
+// delete the pre assigned _id which is assigned by mongoDB and use userId instead
 FavoriteSchema.set("toJSON", {
   transform: function (document, returnedObject) {
     returnedObject.id = returnedObject._id;
