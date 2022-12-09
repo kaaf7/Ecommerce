510b11503b22799913498a25371c104e5ff9e162
kaaf7 <amer.m.karim@gmail.com>
2022-12-09 13:38:16 +0100

    Add Comments and organizing

--- src/Components/VideoPlayer.jsx
+++ src/Components/VideoPlayer.jsx
@@ -1,27 +1,34 @@
-
 /* *👇
  *This is Video Player Component
  *cretaed and styled with Styled Component
- *It will show random fashion videos 
+ *It will show random fashion videos
  *it also directs user to all products when clicked
  */
 
 // import React
 import React from "react";
 
-// import Styled Components
+// impor useState Hook
 import { useState } from "react";
-import styled from "styled-components";
-import { useNavigate } from "react-router-dom";
-import Video1 from "../videos/Video_1.mp4";
 
+// import Styled Components
+import styled from "styled-components";
+
+// import useNavigate to redirect to pages
+import { useNavigate } from "react-router-dom";
+
+import Video1 from "../videos/Video_1.mp4";
 import Video2 from "../videos/Video_2.mp4";
 import Video3 from "../videos/Video_3.mp4";
 import Video4 from "../videos/Video_4.mp4";
 
+// import audio on icon
 import VolumeUpRoundedIcon from "@mui/icons-material/VolumeUpRounded";
+
+// import audio off icon
 import VolumeOffRoundedIcon from "@mui/icons-material/VolumeOffRounded";
 
+// all Components Container
 const Container = styled.div`
   margin-top: 9vh;
   display: flex;
@@ -31,6 +38,7 @@ const Container = styled.div`
   height: 110vh;
   border-bottom: 1px solid lightgray;
 `;
+
 const Wrapper = styled.div`
   height: 100%;
   cursor: pointer;
@@ -39,38 +47,42 @@ const Video = styled.video`
   height: 90%;
   // cursor: pointer;
 `;
+
+// video array
 const VideosArray = [Video1, Video2, Video3, Video4];
+// get random number between 0 and 3 to shoow video
 const randomVideo = Math.floor(Math.random() * 4);
 
+// audio controller container
 const AudioController = styled.div`
+  width: 20px;
+  height: 20px;
   position: absolute;
   left: 8%;
   background-color: none;
   opacity: 1;
   display: flex;
-
   border-radius: 50%;
-  //background-color: #414141;
   align-items: center;
   justify-content: center;
   top: 12%;
-  width: 20px;
-  height: 20px;
   z-index: 1;
   cursor: pointer;
 `;
 
 const VideoPlayer = () => {
+  //useNavigate to navigate pages
   const navigate = useNavigate();
+  // video mute state
   const [muted, setMuted] = useState(true);
-
+  // handleMuted function changes mute state if muted to unmuted and vice versa
   const handleMuted = () => {
     muted ? setMuted(false) : setMuted(true);
-    console.log(muted);
   };
   return (
     <Container>
       <Wrapper>
+        {/* if muted turn icon it into unmuted else turn it into muted*/}
         <AudioController onClick={handleMuted}>
           {muted ? (
             <VolumeOffRoundedIcon sx={{ color: "white" }} />
