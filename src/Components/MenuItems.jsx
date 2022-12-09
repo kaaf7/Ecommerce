e8621a280a3470bc8c4157040a0533765d9e5d05
kaaf7 <amer.m.karim@gmail.com>
2022-12-09 13:34:04 +0100

    Remove unnecessary files

--- src/Components/MenuItems.jsx
+++
@@ -1,9 +0,0 @@
-/* 👇 This is single menu item component
-it is created and styled with Styled Components
-that will be imported and displayed by Navbar 
-*/
-import React from "react";
-
-export const MenuItems = ({ Menu }) => {
-  return <div>{Menu.name}</div>;
-};
