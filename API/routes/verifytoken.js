2da20302b776075adf8b77c4c0dd484ff6a550f5
kaaf7 <amer.m.karim@gmail.com>
2022-12-09 13:39:25 +0100

    Add Comments and organizing verifytoken

--- API/routes/verifytoken.js
+++ API/routes/verifytoken.js
@@ -1,21 +1,33 @@
-const jwToken = require("jsonwebtoken");
+/* * 👇
+ *This verifytoken.js
+ *It is responsible for verifying token on login, updating or deleting data
+ */
 
+// require jason web token
+const jwToken = require("jsonwebtoken");
+// verify token taking request, response, and next middlware arguments
 const verifyJwtToken = (req, res, next) => {
+  // get token from request headers
   const authHeader = req.headers.token;
+  // if token exists then verify it with jsonwebtoken using the secret key in .env
+  // if verified it means that user is the authenticated user
+  // next middlware
   if (authHeader) {
     jwToken.verify(authHeader, process.env.JWT_KEY, (err, user) => {
+      //error 403
       if (err) res.status(403).json("token is not valid");
       req.user = user;
       next();
     });
   } else {
+    // error 401
     return res.status(401).json("you are not authenticated");
   }
 };
-
+// verify if the user the authenticated user
 const verifyTokenAndAuthorization = (req, res, next) => {
   verifyJwtToken(req, res, () => {
-    if (req.user._id === req.params.id /* || req.user.isAdmin*/) {
+    if (req.user._id === req.params.id) {
       next();
     } else {
       res.status(403).json("access is not allowed");
@@ -23,6 +35,7 @@ const verifyTokenAndAuthorization = (req, res, next) => {
   });
 };
 
+// verify if the user is an admin
 const verifyTokenAndAdmin = (req, res, next) => {
   verifyJwtToken(req, res, () => {
     if (req.user.isAdmin) {
