51af86c4c07e7495b693ed31618718a852450701
kaaf7 <amer.m.karim@gmail.com>
2022-12-09 13:48:51 +0100

    Add Comments and organizing

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,22 +1,43 @@
-import React from "react";
+/* * 👇
+ *This is ProductPage
+ *Created and styled with Styled Component
+ *It will display product images and details
+ */
+
+//import react, useState and useEffect hooks
+import { React, useState, useEffect } from "react";
+
+// import styled Components
 import styled from "styled-components";
-import { useLocation } from "react-router-dom";
-import { useState, useEffect } from "react";
+
+// import Navbar
 import { Navbar } from "../Components/Navbar";
+
+// import useLocation to get product id
+import { useLocation } from "react-router-dom";
+
+// import addProdt redcer from cart Slice
 import { addProduct } from "../redux/cartRedux";
+
+// import useDispatch to call reducers
 import { useDispatch } from "react-redux";
+
+// import public requests
 import { publicRequest } from "../services";
+
+// import unique id uuid
 import { v4 as uuidv4 } from "uuid";
 
+// all components container
 const Container = styled.div`
   height: 100vh;
   width: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  background-color: #f9f9f9;
 `;
 
+// items wrapper
 const Wrapper = styled.div`
   height: 90%;
   margin-left: 150px;
@@ -26,20 +47,23 @@ const Wrapper = styled.div`
   align-items: center;
   position: relative;
   gap: 10px;
-  // background-color: lightgrey;
 `;
 
+// product lookbook images wrapper
 const LookBookWrapper = styled.div`
   display: flex;
   gap: 75px;
   flex: 1;
 `;
+
+// lookbook main image
 const LookBook = styled.img`
   height: 80vh;
   width: 30vw;
   object-fit: scale-down;
-  border-color: yellow;
 `;
+
+// look book images container
 const LookBookContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -49,6 +73,8 @@ const LookBookContainer = styled.div`
   width: 75px;
   gap: 2px;
 `;
+
+// lookbook small images
 const LookBookImages = styled.img`
   width: 75px;
   height: 100px;
@@ -58,12 +84,9 @@ const LookBookImages = styled.img`
 
   &:hover {
     transform: scale(1.1);
-    box-shadow: 2.1px 4.2px 4.2px hsl(0deg 0% 0% / 0.44);
-    outline: 0.1px solid lightgray;
-
   }
 `;
-
+// product details
 const ProductInfo = styled.div`
   margin-left: 75px;
   height: 80vh;
@@ -72,24 +95,29 @@ const ProductInfo = styled.div`
   flex-direction: column;
   justify-content: flex-start;
   align-items: start;
-`;
-const Title = styled.h2`
-  margin: 0%;
-`;
-const PriceTag = styled.p`
-  font-weight: 500;
-  font-size: 20px;
-  margin-top: 10px;
-  font-family: Verdana;
-  margin-bottom: 20px;
+  font-family: "Lexend", sans-serif;
 `;
 
+// product name
+const Title = styled.h2`
+  font-weight: 400;
+  margin: 0%;
+`;
+// product price
+const PriceTag = styled.p`
+  //font-weight: 500;
+  font-size: 20px;
+  margin-top: 10px;
+  font-family: "Lexend", sans-serif;
+  margin-bottom: 20px;
+`;
+// colors
 const Color = styled.select`
   height: 40px;
   width: 100%;
-  border: 0.1px solid rgba(231, 77, 77, 0.1);
-  font-family: Tahoma;
-  font-weight: bold;
+  border: 0.1px solid rgba(89, 89, 89, 0.1);
+  font-family: "Lexend", sans-serif;
+  font-weight: 300;
   font-size: 15px;
   &:focus {
     border-bottom: 2px solid black;
@@ -97,55 +125,63 @@ const Color = styled.select`
     border: none;
   }
 `;
-const SizeText = styled.p`
-  font-weight: 400;
-  font-size: 13px;
-`;
-const SizeBlocks = styled.button`
-  height: 35px;
-  width: 35px;
-  border: 0.1px solid rgba(0, 0, 0, 0.2);
-  align-items: center;
-  text-align: center;
-  display: flex;
-  background-color: white;
-  justify-content: center;
-  cursor: pointer;
-  &:focus {
-    border-bottom: 2px solid black;
-  }
-`;
-const SizesContainer = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  gap: 5px;
-`;
+// add to cart button
 const AddToCartBtn = styled.button`
   margin-top: 40px;
   width: 100%;
   padding: 15px;
-  background-color: black;
-  color: white;
+  font-family: "Lexend", sans-serif;
+  border: 0.1px solid black;
+  background-color: #040404;
+  color: #ffffff;
   &:hover {
     background-color: grey;
   }
 `;
-const ProductDescription = styled.p``;
+
+// product details
+const ProductDescription = styled.p`
+  font-family: "Lexend", sans-serif;
+  font-weight: 200;
+`;
 
 const ProductPage = () => {
+  // get location
   const location = useLocation();
+
+  // get product id
   const productId = location.pathname.split("/")[2];
+  // set product according to the response
   const [product, setProduct] = useState();
+  // set product quantity 0 before adding it to cart
   const [quantity, setQuantity] = useState(0);
+  // set product color
   const [color, setColor] = useState();
+  // set main image as nothing
   const [mainImage, setMainImage] = useState("");
+  // dispatch to call redux reducers
   const dispatch = useDispatch();
 
+  // useEffect hook with getProduct function to get a single product with product id dependency
+  useEffect(() => {
+    const getProduct = async () => {
+      try {
+        const res = await publicRequest.get(
+          `/products/allproducts/product?id=${productId}`
+        );
+        setProduct(res.data);
+      } catch (err) {}
+    };
+    getProduct();
+  }, [productId]);
+
+  // handle the main image of product based on imageSrc argument
   const handleMainImage = (imageSrc) => {
     setMainImage(imageSrc);
   };
 
+  /* handleAddProductToCart function is responsible
+   for calling addProduct reducer and dispatching product along with quantity, color and a unique id*/
   const handleAddProductToCart = (e) => {
     e.preventDefault();
     dispatch(
@@ -158,23 +194,13 @@ const ProductPage = () => {
     );
   };
 
-  useEffect(() => {
-    const getProduct = async () => {
-      try {
-        const res = await publicRequest.get(
-          `/products/allproducts/product?id=${productId}`
-        );
-        setProduct(res.data);
-      } catch (err) {}
-    };
-    getProduct();
-  }, [productId]);
   return (
     <Container>
       <Navbar></Navbar>
       <Wrapper>
         <LookBookWrapper>
           <LookBookContainer>
+            {/* on click set the main image as the image being clicked on */}
             {product?.images.map((image, i) => (
               <LookBookImages
                 onClick={() => {
@@ -185,30 +211,36 @@ const ProductPage = () => {
               />
             ))}
           </LookBookContainer>
+          {/*if not main image then show first image*/}
           <LookBook src={!mainImage ? product?.images[0] : mainImage} />
         </LookBookWrapper>
+
         <ProductInfo>
+          {/*product title props */}
           <Title>{product?.productTitle}</Title>
+          {/*product price props */}
           <PriceTag>€ {product?.price}</PriceTag>
-          <Color onChange={(e) => setColor(e.target.value)}>
-            <option selected disabled>
-              SELECT COLOR{" "}
-            </option>
+          {/*product color props */}
+          <Color
+            defaultValue={"DEFAULT"}
+            onChange={(e) => setColor(e.target.value)}
+          >
+            <option
+              value="DEFAULT"
+              style={{ fontFamily: "Lexend,sanSerif" }}
+              disabled
+            >
+              SELECT COLOR
+             </option>
             {product?.colors.map((color) => (
-              <option key={color}>{color}</option>
+              <option key={color}>{color.toUpperCase()}</option>
             ))}
           </Color>
-
-          {/*  
-          <SizeText>SIZES</SizeText>
-         <SizesContainer>
-            <SizeBlocks>1</SizeBlocks>
-            <SizeBlocks>2</SizeBlocks>
-            <SizeBlocks>3</SizeBlocks>
-          </SizesContainer> */}
+          {/*if color is added then call handleAddProductToCart function */}
           <AddToCartBtn onClick={color && handleAddProductToCart}>
-            Add
+            ADD
           </AddToCartBtn>
+          {/*product description props */}
           <ProductDescription>{product?.description}</ProductDescription>
         </ProductInfo>
       </Wrapper>{" "}
