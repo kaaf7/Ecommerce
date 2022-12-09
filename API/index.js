8701e5cc3c5a28c742f0bb62ace9bd0c3fb43a03
kaaf7 <amer.m.karim@gmail.com>
2022-12-09 13:38:29 +0100

    Add Comments and organizing Index

--- API/index.js
+++ API/index.js
@@ -1,25 +1,45 @@
-const express = require("express");
-const app = express();
+/* * 👇
+ *This is serverside index.js
+ *It will connect to PORT and MongoDB server
+ *It will connect all routes with URI
+ */
+
 const mongoose = require("mongoose");
-const userRoute = require("./routes/user");
+// import user rout
+const userRout = require("./routes/user");
+// import auth rout
 const authRout = require("./routes/auth");
+// import product rout
 const productRout = require("./routes/product");
+// import order rout
 const orderRout = require("./routes/order");
+// import cart rout
 const cartRout = require("./routes/cart");
+// import favorite rout
 const favoriteRout = require("./routes/favorite");
-
-const dotenv = require("dotenv");
+// require express
+const express = require("express");
+// call express
+const app = express();
+// require cross origin middleware
 const cors = require("cors");
-dotenv.config();
+// use cors
 app.use(cors());
-const PORT = process.env.PORT;
-const URI = process.env.URI;
+//use express json
 app.use(express.json());
-
+// require dotenv
+const dotenv = require("dotenv");
+// activate dotenv
+dotenv.config();
+// get port from .env
+const PORT = process.env.PORT;
+// get URI from .env
+const URI = process.env.URI;
+// listen to PORT
 app.listen(PORT, () => {
   console.log("connected");
 });
-
+// connect to mongoDB server and console connected
 mongoose
   .connect(URI)
   .then(() => {
@@ -28,12 +48,12 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
+// get api
 app.get("/api", (req, res) => {
   res.send("API");
 });
-
-app.use("/api", userRoute);
+// use all routs
+app.use("/api", userRout);
 app.use("/api/auth", authRout);
 app.use("/api/products", productRout);
 app.use("/api/orders", orderRout);
