4a152293b940282d727aa592109fa205307b57b5
kaaf7 <amer.m.karim@gmail.com>
2022-12-09 13:49:36 +0100

    Add Comments and organizing

--- API/routes/user.js
+++ API/routes/user.js
@@ -1,22 +1,32 @@
+/* * 👇
+ *This user rout will be used in index.js
+ *It register a new user using post method
+ *It is responsible for getting single user
+ *It is responsible for updating or deleting a signle user
+ */
+
+
+
+// require express router
+const router = require("express").Router();
+// import CryptoJS to hash passwords
+const CryptoJS = require("crypto-js");
+// dotenv to hide impoetant keys in .env
+const dotenv = require("dotenv");
+// activate dotenv
+dotenv.config();
+// import User Schema
+const User = require("../models/User");
+// password secret key from .env
+const SECRET_KEY = process.env.SECRET_KEY;
+
+// import verify token, Authorization, and Admin
 const {
   verifyTokenAndAuthorization,
   verifyTokenAndAdmin,
 } = require("./verifytoken");
 
-const CryptoJS = require("crypto-js");
-
-const dotenv = require("dotenv");
-
-const User = require("../models/User");
-
-dotenv.config();
-
-
-const SECRET_KEY = process.env.SECRET_KEY;
-
-const router = require("express").Router();
-
-//GET one user by id
+// get user by id using GET request
 router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
@@ -27,47 +37,7 @@ router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
-// GET ALL USERS
-router.get("/users", verifyTokenAndAdmin, async (req, res) => {
-  const query = req.query.new;
-  try {
-    const users = query
-      ? await User.find().sort({ _id: -1 }).limit(5)
-      : await User.find();
-    res.status(200).json(users);
-  } catch (err) {
-    res.status(500).json("error");
-  }
-});
-
-// Get user stats
-router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
-  const date = new Date();
-  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
-
-  try {
-    const data = await User.aggregate([
-      { $match: { createdAt: { $gte: lastYear } } },
-      {
-        $project: {
-          month: { $month: "$createdAt" },
-        },
-      },
-      {
-        $group: {
-          _id: "$month",
-          total: { $sum: 1 },
-        },
-      },
-    ]);
-    res.status(200).json(data);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-
-//UPDATE
+// update user using PUT request
 router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
   if (req.body.password) {
     req.body.password = CryptoJS.AES.encrypt(
@@ -89,7 +59,7 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
   }
 });
 
-//DELETE
+// delete user using DELETE request
 router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
   try {
     await User.findByIdAndDelete(req.params.id);
