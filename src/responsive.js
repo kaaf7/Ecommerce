33cb1f0ebbbd55a92466afb84810df1aed4468a4
kaaf7 <amer.m.karim@gmail.com>
2022-12-12 20:34:33 +0100

    Add Responsive Features

--- src/responsive.js
+++ src/responsive.js
@@ -2,7 +2,15 @@ import { css } from "styled-components";
 
 export const mobile = (props) => {
   return css`
-    @media only screen and (max-width:414px) {
+    @media only screen and (max-width: 415px) {
+      ${props}
+    }
+  `;
+};
+
+export const tablet = (props) => {
+  return css`
+    @media only screen and (min-width: 415px) and (max-width: 1023px) {
       ${props}
     }
   `;
